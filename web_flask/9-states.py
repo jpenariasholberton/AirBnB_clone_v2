b1bd03449fed403232b548460915fb6860eabae8
jpenariasholberton <1875@holbertonschool.com>
2021-01-20 13:44:18 -0500

    fix exc

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -25,13 +25,6 @@ def states_id(id=None):
     return render_template('9-states.html', states=states)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ list the states """
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
 def close(self):
     """ Remove the current SQLAlchemy Session """
